0833a0b24ed89f4b7561810a13e88d91d4c91331
ilhamaji <iheadskull@gmail.com>
2024-12-07 00:52:24 +0700

    initial commit

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const inputError = require("./src/Exception/inputError");
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 8080,
-    host: process.env.HOST || "localhost",
+    host: process.env.HOST || "0.0.0.0",
     routes: {
       cors: {
-        origin: ["*"], // Mengaktifkan CORS untuk semua origin
+        origin: ["*"],
       },
     },
   });
